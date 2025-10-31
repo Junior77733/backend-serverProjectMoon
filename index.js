5501b9d7bc2fa6f0b1503d510fd3d0a11fd7df6a
Junior77733 <192142025+Junior77733@users.noreply.github.com>
2025-10-31 00:11:24 -0700

    setup basic html and css files to render using middleware and setup the endpoints read for fetching

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 
-const servantRouter = require('./routes/servant');
+app.use(express.static("public"));
 
+
+const servantRouter = require('./routes/servant');
 app.use('/servant', servantRouter);
 
 app.listen(PORT, () => console.log(`API listening at http://localhost:${PORT}`));
